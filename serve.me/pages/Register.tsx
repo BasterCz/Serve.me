abe925177b393b4fa16d410c55a0d29829ba0e13
Diegx-PA <dapadxdxd@gmail.com>
2021-11-11 07:51:07 +0100

    WIP

--- serve.me/pages/Register.tsx
+++ serve.me/pages/Register.tsx
@@ -16,7 +16,6 @@ const Home: NextPage = () => {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
       <main className={styles.main}>
         
         <h1 className={styles.title}>
